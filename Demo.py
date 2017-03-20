dc2b7b0480ddb7b934a354f06d93fbd53e9d1097
Victor Chen <yche276@gmail.com>
2017-03-20 23:32:09 +1300

    learn global keyword

--- Demo.py
+++ Demo.py
@@ -1,2 +1,22 @@
 # python 2.7
+
+import sys
+
 print 'Hello World'
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
+
+
+globvar = 0
+
+def set_globvar_to_one():
+    global globvar    # Needed to modify global copy of globvar
+    globvar = 1
+
+def print_globvar():
+    print(globvar)     # No need for global declaration to read value of globvar
+
+set_globvar_to_one()
+print_globvar()       # Prints 1
