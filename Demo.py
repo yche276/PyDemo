fba58e6112bb0df1db2d1f30c25dc88ca189647f
Victor Chen <yche276@gmail.com>
2017-05-08 16:07:04 +1200

    update

--- Demo.py
+++ Demo.py
@@ -20,3 +20,15 @@ def print_globvar():
 
 set_globvar_to_one()
 print_globvar()       # Prints 1
+
+
+list1 = [2, 3, 5, 7, 11, 13, 17, 19]
+mean = 0.0
+for i in list1:
+    mean = mean + i
+mean /= len(list1)
+
+print ('list1 = %s' % list1)
+print (mean)
+print ('mean = %f, aaaa = %d' % (mean, 10))
+
